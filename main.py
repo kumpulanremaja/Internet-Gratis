de547f2b08d0dbd7353bd5b329dd30811965831f
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2019-05-07 10:25:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ class Server:
         time.sleep(1)
         sys.stderr.write(self.config('Configuration'))
         time.sleep(2)
-        sys.stderr.write(self.log('========Inject Sukses======== \nReading Server:'))
+        sys.stderr.write(self.log('==Inject Sedang Berjalan bye Kumpulan Remaja== \nReading Server:'))
 
     def run(self):
         ServerHandler.LogWindow(True)
